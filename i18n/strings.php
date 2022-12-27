a40f3ff9bf2c3ce0187ec8f4f695bf51d663dc31
developercrystalball <119686124+developercrystalball@users.noreply.github.com>
2022-12-27 17:37:57 +0500

    latest updates

---
+++ i18n/strings.php
@@ -0,0 +1,137 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+__( 'Crystal Ball', 'crystal-ball-insight' );
+__( 'Get aware of any activities that are taking place on your dashboard! Imagine it like a black-box for your WordPress site. e.g. post was deleted, plugin was activated, user logged in or logged out - it\'s all these for you to see.', 'crystal-ball-insight' );
+
+__( 'Administrator', 'crystal-ball-insight' );
+__( 'Editor', 'crystal-ball-insight' );
+
+__( 'Posts', 'crystal-ball-insight' );
+__( 'created', 'crystal-ball-insight' );
+__( 'updated', 'crystal-ball-insight' );
+__( 'deleted', 'crystal-ball-insight' );
+__( 'trashed', 'crystal-ball-insight' );
+__( 'restored', 'crystal-ball-insight' );
+
+__( 'Attachments', 'crystal-ball-insight' );
+__( 'uploaded', 'crystal-ball-insight' );
+
+__( 'Users', 'crystal-ball-insight' );
+__( 'logged_out', 'crystal-ball-insight' );
+__( 'logged_in', 'crystal-ball-insight' );
+__( 'failed_login', 'crystal-ball-insight' );
+__( 'registered', 'crystal-ball-insight' );
+__( 'removed', 'crystal-ball-insight' );
+
+__( 'Plugins', 'crystal-ball-insight' );
+__( 'activated', 'crystal-ball-insight' );
+__( 'deactivated', 'crystal-ball-insight' );
+__( 'installed', 'crystal-ball-insight' );
+__( 'File Updated', 'crystal-ball-insight' );
+
+__( 'Themes', 'crystal-ball-insight' );
+__( 'Theme Customizer', 'crystal-ball-insight' );
+
+__( 'Widgets', 'crystal-ball-insight' );
+
+__( 'Options', 'crystal-ball-insight' );
+
+__( 'Menus', 'crystal-ball-insight' );
+
+__( 'Taxonomies', 'crystal-ball-insight' );
+
+__( 'Core', 'crystal-ball-insight' );
+__( 'WordPress Updated', 'crystal-ball-insight' );
+__( 'WordPress Auto Updated', 'crystal-ball-insight' );
+
+__( 'Comments', 'crystal-ball-insight' );
+__( 'pending', 'crystal-ball-insight' );
+__( 'approved', 'crystal-ball-insight' );
+__( 'unapproved', 'crystal-ball-insight' );
+__( 'untrashed', 'crystal-ball-insight' );
+__( 'spammed', 'crystal-ball-insight' );
+__( 'unspammed', 'crystal-ball-insight' );
+
+__( 'blogname', 'crystal-ball-insight' );
+__( 'blogdescription', 'crystal-ball-insight' );
+__( 'siteurl', 'crystal-ball-insight' );
+__( 'home', 'crystal-ball-insight' );
+__( 'admin_email', 'crystal-ball-insight' );
+__( 'users_can_register', 'crystal-ball-insight' );
+__( 'default_role', 'crystal-ball-insight' );
+__( 'WPLANG', 'crystal-ball-insight' );
+__( 'timezone_string', 'crystal-ball-insight' );
+__( 'date_format', 'crystal-ball-insight' );
+__( 'time_format', 'crystal-ball-insight' );
+__( 'start_of_week', 'crystal-ball-insight' );
+
+__( 'use_smilies', 'crystal-ball-insight' );
+__( 'use_balanceTags', 'crystal-ball-insight' );
+__( 'default_category', 'crystal-ball-insight' );
+__( 'default_post_format', 'crystal-ball-insight' );
+__( 'mailserver_url', 'crystal-ball-insight' );
+__( 'mailserver_login', 'crystal-ball-insight' );
+__( 'mailserver_pass', 'crystal-ball-insight' );
+__( 'default_email_category', 'crystal-ball-insight' );
+__( 'ping_sites', 'crystal-ball-insight' );
+
+__( 'show_on_front', 'crystal-ball-insight' );
+__( 'page_on_front', 'crystal-ball-insight' );
+__( 'page_for_posts', 'crystal-ball-insight' );
+__( 'posts_per_page', 'crystal-ball-insight' );
+__( 'posts_per_rss', 'crystal-ball-insight' );
+__( 'rss_use_excerpt', 'crystal-ball-insight' );
+__( 'blog_public', 'crystal-ball-insight' );
+
+__( 'default_pingback_flag', 'crystal-ball-insight' );
+__( 'default_ping_status', 'crystal-ball-insight' );
+__( 'default_comment_status', 'crystal-ball-insight' );
+__( 'require_name_email', 'crystal-ball-insight' );
+__( 'comment_registration', 'crystal-ball-insight' );
+__( 'close_comments_for_old_posts', 'crystal-ball-insight' );
+__( 'close_comments_days_old', 'crystal-ball-insight' );
+__( 'thread_comments', 'crystal-ball-insight' );
+__( 'thread_comments_depth', 'crystal-ball-insight' );
+__( 'page_comments', 'crystal-ball-insight' );
+__( 'comments_per_page', 'crystal-ball-insight' );
+__( 'default_comments_page', 'crystal-ball-insight' );
+__( 'comment_order', 'crystal-ball-insight' );
+__( 'comments_notify', 'crystal-ball-insight' );
+__( 'moderation_notify', 'crystal-ball-insight' );
+__( 'comment_moderation', 'crystal-ball-insight' );
+__( 'comment_whitelist', 'crystal-ball-insight' );
+__( 'comment_max_links', 'crystal-ball-insight' );
+__( 'moderation_keys', 'crystal-ball-insight' );
+__( 'blacklist_keys', 'crystal-ball-insight' );
+__( 'show_avatars', 'crystal-ball-insight' );
+__( 'avatar_rating', 'crystal-ball-insight' );
+__( 'avatar_default', 'crystal-ball-insight' );
+
+__( 'thumbnail_size_w', 'crystal-ball-insight' );
+__( 'thumbnail_size_h', 'crystal-ball-insight' );
+__( 'thumbnail_crop', 'crystal-ball-insight' );
+__( 'medium_size_w', 'crystal-ball-insight' );
+__( 'medium_size_h', 'crystal-ball-insight' );
+__( 'large_size_w', 'crystal-ball-insight' );
+__( 'large_size_h', 'crystal-ball-insight' );
+__( 'uploads_use_yearmonth_folders', 'crystal-ball-insight' );
+
+__( 'permalink_structure', 'crystal-ball-insight' );
+__( 'category_base', 'crystal-ball-insight' );
+__( 'tag_base', 'crystal-ball-insight' );
+
+__( 'wp_page_for_privacy_policy', 'crystal-ball-insight' );
+
+__( 'Export', 'crystal-ball-insight' );
+__( 'downloaded', 'crystal-ball-insight' );
+
+__( 'Post', 'crystal-ball-insight' );
+__( 'Attachment', 'crystal-ball-insight' );
+__( 'User', 'crystal-ball-insight' );
+__( 'Plugin', 'crystal-ball-insight' );
+__( 'Theme', 'crystal-ball-insight' );
+__( 'Widget', 'crystal-ball-insight' );
+__( 'Menu', 'crystal-ball-insight' );
+__( 'Taxonomy', 'crystal-ball-insight' );
